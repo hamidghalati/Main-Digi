269d70913842489f588c4f6088ab7b5c8da0a9de
hamidghalati <umberela4u@yahoo.com>
2022-08-18 19:22:31 +0430

    Main implement

--- resources/views/include/warranty.blade.php
+++ resources/views/include/warranty.blade.php
@@ -1,5 +1,14 @@
 <form action="{{url('cart')}}" method="post">
     {{csrf_field()}}
+
+
+    <?php
+    $warranty_id=0;
+    $send_time='';
+    $product_price1=0;
+    $product_price2=0;
+    ?>
+
     <ul class="color_ul">
         @foreach($product->getProductColor as $key=>$value)
             @if($color_id==0) <?php if (get_first_color_id($product->getWarranty,$value->getColor->id)) $color_id=$value->getColor->id  ?> @endif
@@ -14,18 +23,17 @@
         @endforeach
     </ul>
     <p class="info_item_product">
-        <?php
-        $warranty_id=0;
-        $send_time='';
-        ?>
 
-        <span class="fa fa-check-square-o"></span>
+
+        <span class="fa fa-check-square"></span>
         @foreach($product->getWarranty as $key=>$value )
             @if($color_id>0)
                 @if($value->color_id==$color_id && $warranty_id==0)
                         <?php
                         $warranty_id=$value->warranty_id;
                         $send_time=$value->send_time;
+                        $product_price1=$value->price1;
+                        $product_price2=$value->price2;
 
                         ?>
                     {{$value->getWarranty->name}}
@@ -34,6 +42,8 @@
                 @if($key==0)
                    <?php
                         $send_time=$value->send_time;
+                        $product_price1=$value->price1;
+                        $product_price2=$value->price2;
                        ?>
 
                     {{$value->getWarranty->name}}
@@ -46,6 +56,9 @@
         @if(!empty($send_time))
             @if($send_time==0)
                 <span>آماده ارسال</span>
+                <span class="fa fa-question-circle" data-toggle="tooltip" data-placement="top"
+                      title="این کالا در انبار فروشنده موجود است.برای ارسال باید تا مدت زمان ذکر شده منتظر بمانید">
+                </span>
             @else
                 <span>ارسال از {{replace_number($send_time)}} روز کاری آینده</span>
                 <span class="fa fa-question-circle" data-toggle="tooltip" data-placement="top"
@@ -54,5 +67,29 @@
             @endif
         @endif
     </p>
+    <input type="hidden" name="warranty_id" id="warranty_id" value="{{$warranty_id}}">
+    <input type="hidden" name="product_id" id="product_id" value="{{$product->id}}">
+
+    <div style="display: inline-flex;width: 100%">
+        <div>
+            @if($product_price2!=$product_price1)
+                <del>{{replace_number(number_format($product_price1))}}</del>
+            @endif
+            <span class="final_product_price">{{replace_number(number_format($product_price2))}}تومان</span>
+        </div>
+
+        @if($product_price2!=$product_price1)
+            <div class="product_discount" data-title="تخفیف">
+                    <?php
+                    $a=($product_price2/$product_price1)*100;
+                    $a=100-$a;
+                    $a=round($a);
+                    ?>
+                {{'%'.replace_number($a)}}
+            </div>
+        @endif
+
+    </div>
+
 </form>
 
